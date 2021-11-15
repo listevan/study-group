67bd290a0137c3950ae910bfcf9fb4b55be1ee18
Steven Li <steven@Stevens-MacBook-Pro.local>
2021-11-14 21:04:51 -0800

    fixed multiple same primes

--- primefactors.py
+++ primefactors.py
@@ -1,6 +1,6 @@
 num = int(input('enter a number: '))
 fin = []
-def factor(num, fin) -> list:
+def factor(num, fin):
     if num == 1:
         fin.append(1)
         fin.sort()
@@ -8,9 +8,10 @@ def factor(num, fin) -> list:
     for i in range(2, int(num)+1):
         prime = True
         for j in (fin):
-            if (i%j == 0):
+            if (i%j == 0 and i != j):
                 prime = False
         if (num%i == 0 and prime):
             fin.append(i)
+            print(fin)
             return(factor(num/i, fin))
 print(factor(num, fin))
