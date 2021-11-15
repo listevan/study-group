02028fcd37aa46d964f60e594a9408d08ad93933
Steven Li <steven@Stevens-MacBook-Pro.local>
2021-11-15 12:36:20 -0800

    implemented factors function into factor

--- polyfactor.py
+++ polyfactor.py
@@ -5,16 +5,15 @@ c = int(input('Give C: '))
 
 def factors(num) -> list:
     fin = []
+    num = abs(num)
     for i in range(1, num+1):
         if (num%i==0):
             fin.append(i)
             fin.append(-i)
     return fin
 
-afactors = factors(a)
-cfactors = factors(c)
-
-def factor(afactor, cfactor, a, b, c):
+def factor(a, b, c):
+    afactor, cfactor = factors(a), factors(c)
     for i in (afactor):
         for j in (cfactor):
             if (c-(b*j/i)+(a*((j/i)**2))==0):
@@ -22,4 +21,4 @@ def factor(afactor, cfactor, a, b, c):
                     return ("(ax+b)(cx+d) a: " + str(i) + " b: " + str(j) + " c: " + str(int(a/i)) + " d: " + str(int((b-a*(j/i))/i)))
                 return ("(ax+b)(cx+d) a: 1" + " b: " + str(j/i) + " c: " + str(int(a)) + " d: " + str(int(b-a*(j/i))))
     return("unfactorable")
-print(factor(afactors, cfactors, a, b, c))            
+print(factor(a, b, c))            
